d934e4e472abcce45a2141cef270f70ea349a08e
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-13 00:24:50 +0900

    RequiredArgsContructor - final simple code

--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -16,16 +16,6 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    /*@Autowired //생성자가 하나면 생략 가능하다. --> @RequiredArgsContructor가 final 이 붙은 필드를 바탕으로 생성자를
-    자동으로 만들어 준다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-
-        System.out.println("memberRepository= "+memberRepository);
-        System.out.println("discountPolicy= "+discountPolicy);
-    }*/
-
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);

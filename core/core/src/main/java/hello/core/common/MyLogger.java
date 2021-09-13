e903a9706de9ebcfe279629d787bd7feb855fc5e
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-09-13 12:18:22 +0900

    logger22

--- core/core/src/main/java/hello/core/common/MyLogger.java
+++ core/core/src/main/java/hello/core/common/MyLogger.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
@@ -22,6 +23,11 @@ public class MyLogger {
 
     @PostConstruct
     public void init(){
-        String uuid= UUID.randomUUID().toString();
+        uuid= UUID.randomUUID().toString();
+        System.out.println("["+uuid+"] request scope bean create:" + this);
+    }
+    @PreDestroy
+    public void close(){
+        System.out.println("["+uuid+"] request scope bean close:" + this);
     }
 }
